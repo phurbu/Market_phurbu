f577b6a08e9928b59ca7b067fafe01999aac7e5d
dan Yahoo <danielo@yahoo-inc.com>
2014-07-15 08:51:35 -0700

    Merge branch 'master' of https://github.com/phurbu/Market_phurbu
    Left out Conflicts:
    Market.xcodeproj/project.pbxproj
    Market/AppDelegate.m

---
+++ Market/CameraViewController.h
@@ -0,0 +1,13 @@
+//
+//  CameraViewController.h
+//  Market
+//
+//  Created by Kaili An on 7/15/14.
+//  Copyright (c) 2014 Phurbu Dolma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CameraViewController : UIViewController
+
+@end
